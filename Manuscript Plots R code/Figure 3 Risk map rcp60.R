30b815ae2561054e65668924bd3c69f7156438a8
AlkeVoskamp <alke.voskamp@senckenberg.de>
2020-09-17 13:33:59 +0200

    Formatting for submission

--- Manuscript Plots R code/Figure 3 Risk map rcp60.R
+++ Manuscript Plots R code/Figure 3 Risk map rcp60.R
@@ -8,15 +8,10 @@ rm(list=ls())
 
 #-#-# Load the libraries #-#-#
 library(ggplot2)
-# library(lattice)
-# library(rasterVis)
-# library(maps)
 library(grid)
 library(gridExtra)
 library(rgdal)
-# library(RColorBrewer)
-# library(colorspace)
-# library(dplyr)
+
 
 #-#-# Get the data #-#-#
 setwd("/Users/alkevoskamp/Documents/PD manuscript/Data for plots/")
@@ -41,7 +36,6 @@ CombConSc <- CombCon[c("x","y","PDpropChange","MPDpropChange","Risk_categories")
 
 CombConSc <- na.omit(CombConSc)
 
-#write.csv(CombCon,"PDandMPD_random_changes_disp2_rcp60_2080_RC_Final_Missing_Continent.csv")
 
 #-#-# Worldmap (global and cropped) #-#-#
 setwd("/Users/alkevoskamp/Documents/PD manuscript/Data for plots/ne_50m_admin_0_countries/")
@@ -59,10 +53,10 @@ SRscatter <- ggplot(CombConSc,aes(x=PDpropChange,y=MPDpropChange,col=Risk_catego
   annotate("rect",xmin = 0, xmax = 200, ymin = -35, ymax = 0, fill = "indianred1", alpha = 0.8)+
   annotate("rect",xmin = 0, xmax = 200, ymin = 0, ymax = 40, fill = "yellow2", alpha = 0.8)+
   annotate("rect",xmin = -200, xmax = 0, ymin = 0, ymax = 40, fill = "turquoise1", alpha = 0.8)+
-  annotate("text", x = 100, y = 30, label = "Increasing\ndiversification", colour = "black",size=9)+
-  annotate("text", x = 100, y = -25, label = "Increasing\nclustering", colour = "black",size=9)+
-  annotate("text", x = -100, y = -25, label = "Increasing\nhomogenisation", colour = "black",size=9)+
-  annotate("text", x = -100, y = 30, label = "Increasing\noverdispersion", colour = "black",size=9)+
+  annotate("text", x = 100, y = 30, label = "Increasing\ndiversification", colour = "black",size=7)+
+  annotate("text", x = 100, y = -25, label = "Increasing\nclustering", colour = "black",size=7)+
+  annotate("text", x = -100, y = -25, label = "Increasing\nhomogenisation", colour = "black",size=7)+
+  annotate("text", x = -100, y = 30, label = "Increasing\noverdispersion", colour = "black",size=7)+
   geom_point(alpha = 1,shape=1) +
   theme(panel.background = element_blank(),
         axis.text.x = element_text(size=22),
@@ -75,24 +69,16 @@ SRscatter <- ggplot(CombConSc,aes(x=PDpropChange,y=MPDpropChange,col=Risk_catego
   guides(colour = guide_legend(override.aes = list(size=8,fill=NA)))+
   geom_hline(yintercept=0, linetype="dashed", color= "black")+
   geom_vline(xintercept=0, linetype="dashed", color= "black")+
-  labs(x="% change in PD\n", y="\n% change in MPD", title="")+
+  labs(x="% change in Faith PD\n", y="\n% change in MPD", title="")+
   ggtitle("(a)")+ 
   theme(plot.title = element_text(size = 21,face="bold",hjust = 0))
 plot(SRscatter)
 
-#ggsave("Figure 2 Part 1 PD vs MPD change scatter rcp60.tiff",SRscatter,width=24, height=14, unit="in", dpi=600, bg="transparent")
 
 #-#-# Map %PD vs %MPD change #-#-#
 binaryColors <- c("indianred1","yellow2","mediumpurple1","turquoise1")
 #down right, up right, down left, up left
 
-## Rearrange the edge coordinates to fit country boarder map
-# RPDchangeC1 <- subset(RPDchange,x > -170) ## Subset to x coords higher than -170 these are fine
-# RPDchangeC2 <- subset(RPDchange,x <= -170) ## Subset to x coords lower than -170 these need to move
-# RPDchangeC2$x <- abs(RPDchangeC2$x) + 10 ## Make coords positive to shift to other side and add 10 degree to attach end of map (179.5 center)  
-# x <- rev(RPDchangeC2$x) ## Reorder the x coords because they need to be mirrored to be attached on the other side
-# RPDchangeC2$x <- x ## Replace with the reordered x coords
-# RPDchangeC3 <- rbind(RPDchangeC1,RPDchangeC2) ##Merge the two dataframes back together
 
 ## Set alpha
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
@@ -124,9 +110,6 @@ Final <- arrangeGrob(SRscatter,RiskMap,
                      ncol = 2,
                      nrow = 1)
 plot(Final)
-
-#setwd("/Users/alkevoskamp/Documents/PD manuscript/Manuscript Figures/")
-#ggsave("Figure 3 PD vs MPD change map rcp60.tiff",Final,width=18, height=13, unit="in", dpi=600, bg="transparent")
 
 
 #-#-# Percentages No of assemblages in different risk categories for text #-#-#
@@ -191,7 +174,7 @@ plot(BarPlot)
 #-#-# Combine 3 plots #-#-#
 FinalComb <- grid.arrange(
               arrangeGrob(SRscatter,RiskMap,
-                     widths = c(6,8),
+                     widths = c(4,8),
                      heights = c(1),
                      ncol = 2,
                      nrow = 1),
@@ -200,11 +183,11 @@ FinalComb <- grid.arrange(
                           heights = c(8),
                           ncol = 1,
                           nrow = 1),
-              nrow=2, ncol =1, heights=c(4,1)
+              nrow=2, ncol =1, heights=c(4,2)
 )
 plot(FinalComb)
 
 
 setwd("/Users/alkevoskamp/Documents/PD manuscript/Manuscript Figures/")
-ggsave("Figure 3 PD vs MPD change map rcp60.tiff",FinalComb,width=20, height=10, unit="in", dpi=600, bg="transparent")
+ggsave("Figure 3 PD vs MPD change map rcp60.tiff",FinalComb,width=18, height=8, unit="in", dpi=600, bg="transparent")
 
