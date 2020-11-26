dbf32a8bdd1c5cd01adf781f1dfc35fde6bdf254
AlkeVoskamp <alke.voskamp@senckenberg.de>
2020-11-26 15:53:44 +0100

    Adapted figure legends to decrease and increase

--- Manuscript Plots R code/Spp Figure 4 non random changes rcp60.R
+++ Manuscript Plots R code/Spp Figure 4 non random changes rcp60.R
@@ -22,7 +22,7 @@ library(rgdal)
 
 #-#-# Get and prepaire the plot data #-#-#
 setwd("/Users/alkevoskamp/Documents/PD manuscript/Data for plots/")
-CombCon <- read.csv("PDandMPD_random_changes_disp1_rcp60_2080_Final.csv")
+CombCon <- read.csv("PDandMPD_random_changes_low_dispersal_rcp60_2080.csv")
 
 ## Add actual vs random comparison columns
 CombCon$PDInvsRIn <- CombCon$FaithACIn - CombCon$FaithInRmean
@@ -81,7 +81,7 @@ bl1 <- colorRampPalette(rev(c("lightskyblue","royalblue","navy")))(576)  #931.3 
 re1 <- colorRampPalette(rev(c("darkred","red2","mistyrose")))(530)       #1026.5   538
 out <- ggplot(data=PDRLoss, aes(y=y, x=x)) +
   geom_raster(aes(fill =  PDOutvsROut), stat = "identity", position = "identity", hjust = 0, vjust = 0, interpolate = FALSE,)+
-  scale_fill_gradientn("Projected PD loss\n - random PD loss\n",colours=c(bl1,wi1,re1),limits=c(-576,530),breaks=c(-576,530),labels=c("LESS LOSS\nthan random","MORE LOSS\nthan random"))+ # Insert colour and set range
+  scale_fill_gradientn("Projected PD change\n - random PD change\n",colours=c(bl1,wi1,re1),limits=c(-553,538),breaks=c(-553,538),labels=c("Stronger decrease\n than random","Weaker decrease\n than random"))+ # Insert colour and set range
   theme(legend.position = c(0.5, 0.6))+ # Positioning the legend <- needs to run with legend position to extract legend  
   theme(legend.text=element_text(size=23),legend.title=element_text(size=28))+ # Change font size legend
   theme(legend.key.size = unit(0.5, "cm"),legend.key.height = unit(0.5, "cm")) # Change size of legend key
@@ -126,7 +126,7 @@ wi1 <- colorRampPalette(rev(c("gray82","gray90","gray82")))(4)
 
 In <- ggplot(data=PDRGain, aes(y=y, x=x)) +
   geom_raster(aes(fill = PDInvsRIn), stat = "identity", position = "identity", hjust = 0, vjust = 0, interpolate = FALSE,)+
-  scale_fill_gradientn("Projected PD gain\n - random PD gain\n",colours=c(re1,bl1),limits=c(-593,491),breaks=c(-593,491),labels=c("LESS GAIN\nthan random","MORE GAIN\nthan random"))+ # Insert colour and set range
+  scale_fill_gradientn("Projected PD change\n - random PD change\n",colours=c(re1,bl1),limits=c(-965,602),breaks=c(-965,602),labels=c("Weaker increase\n than random","Stronger increase\n than random"))+ # Insert colour and set range
   theme(legend.position = c(0.5, 0.6))+ # Positioning the legend <- needs to run with legend position to extract legend  
   theme(legend.text=element_text(size=23),legend.title=element_text(size=28))+ # Change font size legend
   theme(legend.key.size = unit(0.5, "cm"),legend.key.height = unit(0.5, "cm")) # Change size of legend key
@@ -177,7 +177,7 @@ re1 <- colorRampPalette(rev(c("darkred","red2","mistyrose")))(35)       #1026.5
 wi1 <- colorRampPalette(rev(c("gray82","gray90","gray82")))(0)
 out <- ggplot(data=MPDRLoss, aes(y=y, x=x)) +
   geom_raster(aes(fill =  PDOutvsROut), stat = "identity", position = "identity", hjust = 0, vjust = 0, interpolate = FALSE,)+
-  scale_fill_gradientn("Projected MPD loss\n - random MPD loss\n",colours=c(bl1,wi1,re1),limits=c(-79,35),breaks=c(-79,35),labels=c("LESS LOSS\nthan random","MORE LOSS\nthan random"))+ # Insert colour and set range
+  scale_fill_gradientn("Projected MPD change\n - random MPD change\n",colours=c(bl1,wi1,re1),limits=c(-79,36),breaks=c(-79,36),labels=c("Weaker decrease\n than random","Stronger decrease\n than random"))+ # Insert colour and set range
   theme(legend.position = c(0.5, 0.6))+ # Positioning the legend <- needs to run with legend position to extract legend  
   theme(legend.text=element_text(size=23),legend.title=element_text(size=28))+ # Change font size legend
   theme(legend.key.size = unit(0.5, "cm"),legend.key.height = unit(0.5, "cm")) # Change size of legend key
@@ -221,7 +221,7 @@ re1 <- colorRampPalette(rev(c("mistyrose","red2","darkred")))(23)       #1026.5
 wi1 <- colorRampPalette(rev(c("gray82","gray90","gray82")))(0)
 In <- ggplot(data=MPDRGain, aes(y=y, x=x)) +
   geom_raster(aes(fill = PDInvsRIn), stat = "identity", position = "identity", hjust = 0, vjust = 0, interpolate = FALSE,)+
-  scale_fill_gradientn("Projected MPD gain\n - random MPD gain\n",colours=c(re1,wi1,bl1),limits=c(-23,36),breaks=c(-23,36),labels=c("LESS GAIN\nthan random","MORE GAIN\nthan random"))+ # Insert colour and set range
+  scale_fill_gradientn("Projected MPD change\n - random MPD change\n",colours=c(re1,wi1,bl1),limits=c(-18,33),breaks=c(-18,33),labels=c("Weaker increase\n than random","Stronger increase\n than random"))+ # Insert colour and set range
   theme(legend.position = c(0.5, 0.6))+ # Positioning the legend <- needs to run with legend position to extract legend  
   theme(legend.text=element_text(size=23),legend.title=element_text(size=28))+ # Change font size legend
   theme(legend.key.size = unit(0.5, "cm"),legend.key.height = unit(0.5, "cm")) # Change size of legend key
@@ -234,7 +234,7 @@ plot(legendPlot4)
 
 #-#-# Combine plot random PD loss and gain #-#-#  
 CombNull <- arrangeGrob(plot1,legendPlot1,plot2,legendPlot2,plot3,legendPlot3,plot4,legendPlot4,
-                        widths = c(4,1,4,1),
+                        widths = c(4,1.1,4,1.1),
                         heights = c(0.3,0.3),
                         ncol = 4,
                         nrow = 2)
@@ -245,7 +245,7 @@ plot(CombNull)
 
 
 setwd("/Users/alkevoskamp/Documents/PD manuscript/Manuscript Figures/")
-ggsave("Spp Figure 4 Random_PD and_MPD_change_rcp60_significant_cellsLow_disp.tiff",CombNull,width=35, height=13, unit="in", dpi=300, bg="transparent")
-ggsave("Spp Figure 4 Random_PD and_MPD_change_rcp60_significant_cellsLow_disp.png",CombNull,width=24, height=13, unit="in", dpi=600, bg="transparent")
+ggsave("Spp Figure 7 Random_PD and_MPD_change_rcp60_significant_cells_low_disp.tiff",CombNull,width=35, height=13, unit="in", dpi=300, bg="transparent")
+#ggsave("Spp Figure 4 Random_PD and_MPD_change_rcp60_significant_cellsLow_disp.png",CombNull,width=24, height=13, unit="in", dpi=600, bg="transparent")
 
 
